08705f2e52ff3baa59e793fc1e378620ef3bd0b2
budou114 <58349030+budou114@users.noreply.github.com>
2024-07-23 16:40:22 +0900

    22. 投稿用タイムラインを作成してみよう

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Timeline from "@/components/Timeline";
 import Head from "next/head";
 
 export default function Home() {
@@ -9,6 +10,9 @@ export default function Home() {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+            <div>
+                <Timeline />
+            </div>
         </>
     );
 }
