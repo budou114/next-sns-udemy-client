d9c44f9810c54eb4854ed97a4b608c8487940f0f
budou114 <58349030+budou114@users.noreply.github.com>
2024-08-17 10:25:31 +0900

    51. 実際にユーザーアイコンを生成してみよう

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { PostType } from './types';
+import Image from 'next/image';
 
 type Props = {
     post: PostType;
@@ -12,9 +13,9 @@ const Post = (props: Props) => {
         <div className="bg-white shadow-md rounded p-4 mb-4">
             <div className="mb-4">
                 <div className="flex items-center mb-2">
-                    <img
+                    <Image
                         className="w-10 h-10 rounded-full mr-2"
-                        src="https://via.placeholder.com/150"
+                        src={post.author.profile?.profileImageUrl}
                         alt="User Avatar"
                     />
                     <div>
